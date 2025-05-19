c9dc2d0318b8c380214437822c8083c87e9d499a
jerryxgh <gh_xu@qq.com>
2025-05-19 20:33:03 +0800

    ! try treesit again

--- lambda-treesit.el
+++ lambda-treesit.el
@@ -38,7 +38,7 @@
   (treesit-auto-install 'prompt)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode)
+  ;; (global-treesit-auto-mode)
   (add-hook 'go-ts-mode-hook
             (lambda ()
               (setq tab-width 4)
