f2e134e56ac3765ab45edba0c10f632f2cfe5c79
guanghui.xgh <gh_xu@qq.com>
2026-09-03 15:06:35 +0800

    ! support C-c C-f pretty json in json-ts-mode

--- lambda-json.el
+++ lambda-json.el
@@ -5,6 +5,7 @@
 ;;; Code:
 
 (require 'lambda-core)
+(require 'json)
 
 ;; json-mode -------------------------------------------------------------------
 (use-package json-mode
@@ -13,6 +14,20 @@
   (json-mode-indent-level 4)
   ;; :config
   )
+
+(setq json-encoding-default-indentation "  ")
+
+(defun lambda--json-format ()
+  "Format the active JSON region, or the whole buffer."
+  (interactive)
+  (if (use-region-p)
+      (json-pretty-print (region-beginning) (region-end))
+    (json-pretty-print-buffer)))
+
+(with-eval-after-load 'json-ts-mode
+  (define-key json-ts-mode-map
+              (kbd "C-c C-f")
+              #'lambda--json-format))
 
 (provide 'lambda-json)
 
